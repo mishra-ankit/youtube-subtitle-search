2867e58c6e0cf84a0c38fcd1892edea7346651ce
Ankit Mishra <anmishr3@cisco.com>
2022-09-27 08:25:23 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,115 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { stat } from 'fs';
+
+type SentenceInfo = { start: number, text: string };
+type VideoInfo = {
+  formats: [
+    {
+      url: string
+    }
+  ],
+  tracks: [
+    {
+      languageCode: string,
+      baseUrl: string
+    }
+  ]
+};
+
+async function getVideoInfo(input: string): Promise<VideoInfo> {
+  const url = `https://youtube-video-info-fvxkv650trvd.runkit.sh/?url=${input}`;
+  const videoInfo = await fetch(url).then(t => t.json()) as VideoInfo;
+  return videoInfo;
+}
+
+async function getSubtitle(url: string): Promise<SentenceInfo[]> {
+  const resp = await fetch(url)
+    .then(response => response.text())
+    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+
+  const sentenceNodes = Array.from(resp.getElementsByTagName("text"));
+
+  return sentenceNodes.map(sentence => ({
+    start: parseFloat(sentence.getAttribute("start") as string),
+    duration: parseFloat(sentence.getAttribute("dur") as string),
+    text: sentence.textContent as string
+  }));
+}
 
 function App() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [state, setState] = useState<{
+    videoURL: string,
+    subtitle?: SentenceInfo[],
+  }>();
+
+  const handleVideoSearch = async () => {
+    const val = inputRef.current?.value;
+    console.log(val);
+    if (val) {
+      const info = await getVideoInfo(val);
+
+      const videoURL = info.formats[0].url;
+      setState({ videoURL });
+
+      // TODO: Handle better
+      const subtitle = await getSubtitle(info.tracks[0].baseUrl);
+      setState({ videoURL, subtitle });
+
+    } else {
+      // TODO: handle 
+    }
+  }
+
+  useEffect(() => {
+    if (state?.videoURL && videoRef.current) {
+      videoRef.current.src = state?.videoURL;
+    }
+  }, [state?.videoURL]);
+
+  const handleSentenceClick = (sentenceInfo: SentenceInfo) => {
+    console.log(sentenceInfo);
+    if (videoRef.current) {
+      videoRef.current.currentTime = sentenceInfo.start;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <nav>
+        <a href="/">
+          <img src={logo} className="logo" alt="logo" />
         </a>
-      </header>
-    </div>
+      </nav>
+      <section className='container'>
+        <header>
+          <div className="search-grid">
+            <input type="text" ref={inputRef} />
+            <button type="submit" onClick={handleVideoSearch}>Go</button>
+          </div>
+        </header>
+
+        {state && <main>
+          <div className="main-grid">
+            <video controls ref={videoRef}></video>
+            <section>
+              <input type="search" id="search" name="search" placeholder="Search" />
+            </section>
+          </div>
+
+          <section>
+            {state.subtitle?.map((sentenceInfo, index) => {
+              return <span key={index} className='sentence' onClick={() => {
+                handleSentenceClick(sentenceInfo);
+              }}>{sentenceInfo.text + " "}</span>
+            })}
+          </section>
+        </main>}
+      </section>
+    </>
   );
 }
 
