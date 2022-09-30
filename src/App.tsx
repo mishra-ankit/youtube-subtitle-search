9b7d832d67bdc3f2991325e5335f9db5a5297128
Ankit Mishra <anmishr3@cisco.com>
2022-09-30 08:34:59 +0200

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,15 @@ function App() {
   const [state, setState] = useState<{
     rawVideoURL?: string,
     subtitle?: SentenceInfo[],
-    activeSentenceIndex?: number
-  }>();
+    activeSentenceIndex: number
+  }>({activeSentenceIndex: 0});
 
   const handleVideoSearch = async () => {
     const val = inputURL.current?.value;
     if (val) {
       const info = await getVideoInfo(val);
       const rawVideoURL = info.formats[0].url;
-      setState({ rawVideoURL });
+      setState((curr) => ({ ...curr, rawVideoURL }));
 
       // TODO: Handle better
       const subtitle = await getSubtitle(info.tracks[0].baseUrl);
@@ -64,7 +64,7 @@ function App() {
         <header>
           <div className="search-grid">
             <input name="videoId" type="text" ref={inputURL} placeholder="Put youtube URL or video ID here" autoFocus />
-            <button type="submit" onClick={handleVideoSearch}>Go</button>
+            <button type="submit" onClick={handleVideoSearch}>Go gu</button>
           </div>
         </header>
 
@@ -73,19 +73,29 @@ function App() {
             <video controls ref={videoRef} autoPlay onTimeUpdate={handleTimeUpdate}></video>
           </div>
 
-          <section>
-            {state.subtitle?.map((sentenceInfo, index) => {
-              return <span key={index}
-                className={'sentence ' + (state.activeSentenceIndex === index ? "active" : "")}
-                onClick={() => {
-                  handleSentenceClick(sentenceInfo, index);
-                }}>{sentenceInfo.text + " "}</span>
-            })}
-          </section>
+          <Subtitle handleSentenceClick={handleSentenceClick} subtitle={state.subtitle} activeSentenceIndex={state.activeSentenceIndex} />
         </main>}
       </section>
     </>
   );
+}
+
+function Subtitle({ handleSentenceClick, subtitle, activeSentenceIndex }:
+  {
+    handleSentenceClick: (sentenceInfo: SentenceInfo, index: number) => void,
+    subtitle?: SentenceInfo[];
+    activeSentenceIndex: number;
+  }
+) {
+  return <section>
+    {subtitle?.map((sentenceInfo, index) => {
+      return <span key={index}
+        className={'sentence ' + (activeSentenceIndex === index ? "active" : "")}
+        onClick={() => {
+          handleSentenceClick(sentenceInfo, index);
+        }}>{sentenceInfo.text + " "}</span>;
+    })}
+  </section>;
 }
 
 export default App;
